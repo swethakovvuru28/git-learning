03e4cf4bd2ec598b53ef5c11f9543d26e2858c9c
swetha <swethakovvuru28@gmail.com>
2021-06-25 09:01:26 +0530

    Java programs

---
+++ Day25/Switchcase.java
@@ -0,0 +1,28 @@
+package Day25;
+
+public class Switchcase {
+  public static void main(String[] args)
+  {
+	   int day=0;
+	  
+	  switch(day)
+      {
+      case 0:System.out.println("Monday");
+             break;
+      case 1:System.out.println("Tuesday");
+             break;
+      case 2:System.out.println("Wednesday");
+             break;       
+      case 3:System.out.println("Thursday");
+             break;
+      case 4:System.out.println("friday");
+             break;
+      case 5:System.out.println("saturday");
+             break;
+      case 6:System.out.println("sunday");
+             break;
+      }
+  }
+}
+	  
+	  
